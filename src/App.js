f050f202ed1edff080aa2afefad8975798dfd4f8
KAZI-SAMIR <kazi46617@gmail.com>
2022-09-24 18:12:18 +0600

    added again

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p
+        
+        > this is a page of kazi samir finallyy  </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
