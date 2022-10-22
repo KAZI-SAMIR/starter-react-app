589f9b1ac99c0586118243b5c4e1ca020e87fc4c
KAZI-SAMIR <kazi46617@gmail.com>
2022-10-22 12:55:21 +0600

    jsx added

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express()
 var options = {
   dotfiles: 'ignore',
   etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+  extensions: ['htm', 'html','css','js','jsx','ico','jpg','jpeg','png','svg'],
   index: ['index.html'],
   maxAge: '1m',
   redirect: false
